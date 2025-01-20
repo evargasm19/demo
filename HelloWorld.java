5a9925b1cebae5418fb3b28262e629b2d7c698af
evargasm19 <evargasm19@gmail.com>
2025-01-20 14:05:23 -0500

    developer1

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,12 +6,27 @@ class HelloWorldImplementation {
         String hello ="Cordial greetings";
         return hello;
     }
+
+    int sum(){
+        int a=5, b=5;
+        int sum = a + b;
+        return sum;
+    }
+
+    int division(){
+        int a=9, b=6;
+        int div = a / b;
+        return div;
+    }
+
 }
 
 public class HelloWorld {
 	public static void main(String[] args) {
 	HelloWorldImplementation varobj = new HelloWorldImplementation();
     System.out.println(varobj.greeting());
+    System.out.println(varobj.sum());
+    System.out.println(varobj.division());
 	}
 
 }
