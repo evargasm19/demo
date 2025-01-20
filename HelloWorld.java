cd77adeadf07445d307ea43c1e68a39a5f35bfb1
evargasm19 <evargasm19@gmail.com>
2025-01-20 14:03:19 -0500

    developer2

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,12 +6,19 @@ class HelloWorldImplementation {
         String hello ="Cordial greetings";
         return hello;
     }
+
+    double fraction(){
+        double a=9.4, b=3.66767, c;
+        return c = a/b;
+    }
+
 }
 
 public class HelloWorld {
 	public static void main(String[] args) {
 	HelloWorldImplementation varobj = new HelloWorldImplementation();
     System.out.println(varobj.greeting());
+    System.out.println(varobj.fraction());
 	}
 
 }
